9b010f7403abe6b65dc1a4d3d11fc7b84c1aaabd
H S <hsekha@gmail.com>
2019-10-20 13:37:04 -0700

    New branch for Lab 3.

--- MahlerFanSite/Controllers/HomeController.cs
+++ MahlerFanSite/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace MahlerFanSite.Controllers
     {
         public HomeController()
         {
+            // Some stories to fill our repository for testing
             if (StoryRepository.Stories.Count == 0)
             {
                 Story story = new Story()
@@ -16,6 +17,20 @@ namespace MahlerFanSite.Controllers
                     StoryId = 1,
                     Text = "Mahler once slept the whole day!",
                     PublishedDate = new DateTime(2012, 6, 12)
+                };
+                StoryRepository.AddStory(story);
+                story = new Story()
+                {
+                    StoryId = 2,
+                    Text = "I met Mahler once!",
+                    PublishedDate = new DateTime(2002, 6, 12)
+                };
+                StoryRepository.AddStory(story);
+                story = new Story()
+                {
+                    StoryId = 3,
+                    Text = "Once upon a tie...",
+                    PublishedDate = new DateTime(2012, 11, 12)
                 };
                 StoryRepository.AddStory(story);
             }
@@ -28,7 +43,11 @@ namespace MahlerFanSite.Controllers
         public IActionResult Stories()
         {
             List<Story> stories = StoryRepository.Stories;
-            return View(stories);
+
+            // Stories are now sorted alphabetically by the text of the story
+            stories.Sort((a, b) => string.Compare(a.Text, b.Text, StringComparison.Ordinal));
+
+            return View("Stories", stories);
         }
         
         public IActionResult AddStory() => View();
