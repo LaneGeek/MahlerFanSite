772f8707c4cbcfd9ed62218755a015c123c2c442
H S <sekhah@my.lanecc.edu>
2019-10-21 18:09:08 -0700

    Added more extension methods including Average() to get an average rating for stories rated.

--- MahlerFanSite/Models/ExtensionMethods.cs
+++ MahlerFanSite/Models/ExtensionMethods.cs
@@ -4,6 +4,19 @@ namespace MahlerFanSite.Models
 {
     public static class ExtensionMethods
     {
+        // These two methods will work any List because of the use of Generics
         public static bool IsEmpty<T>(this List<T> list) => list.Count == 0;
+        public static bool IsNotEmpty<T>(this List<T> list) => list.Count > 0;
+
+        // This method will only work with a List of integers
+        public static double Average(this List<int> list)
+        {
+            int sum = 0;
+            foreach (int number in list)
+            {
+                sum += number;
+            }
+            return (double) sum / list.Count;
+        }
     }
 }
