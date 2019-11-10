6c030b86b7ef97fdd2fce0606c897d4243803040
H S <sekhah@my.lanecc.edu>
2019-11-10 13:39:38 -0800

    Lab 6 completed.

---
+++ MahlerFanSite/Controllers/ExtrasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MahlerFanSite.Controllers
+{
+    public class ExtrasController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public RedirectResult Index(string url) => Redirect(url);
+
+        public BadRequestResult Bad() => BadRequest();
+
+        public JsonResult Json()
+        {
+            // I am creating a dynamic object, similar to JavaScript's object
+            dynamic json = new
+            {
+                UserId = 1,
+                UserName = "John",
+                Company = "Google",
+                Salary = 500
+            };
+            return Json(json);
+        }
+
+        public string GetString() => "This is just a plain old boring string and nothing more!";
+    }
+}
